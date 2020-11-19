b95f920a210b85ebb0d48b9a7a5514634990cce7
胡争辉 <hu@daonao.com>
2020-11-20 00:52:14 +0800

    hello world

--- hello-world/hello-world/src/main/java/com/example/helloworld/DemoApplication.java
+++ hello-world/hello-world/src/main/java/com/example/helloworld/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("hello")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("world")
+	public String world() {
+		return "Hello, World!";
+	}
 }
