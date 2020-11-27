ef76be772a55b394a1285c421d05583c57379073
胡争辉 <hu@daonao.com>
2020-11-27 21:44:58 +0800

    mvn javadoc:test-fix -Dforce=true

--- maven-javadoc-plugin/use-maven-javadoc-plugin/src/main/java/com/example/usemavenjavadocplugin/DemoApplication.java
+++ maven-javadoc-plugin/use-maven-javadoc-plugin/src/main/java/com/example/usemavenjavadocplugin/DemoApplication.java
@@ -3,9 +3,20 @@ package com.example.usemavenjavadocplugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>DemoApplication class.</p>
+ *
+ * @author huzhenghui
+ * @version $Id: $Id
+ */
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * <p>main.</p>
+	 *
+	 * @param args an array of {@link java.lang.String} objects.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
